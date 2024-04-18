edd17457a697bd87ce7dde7997075f219abcf104
samyak-anand <samyak.annad@gmail.com>
2024-04-18 21:42:11 +0200

    update

---
+++ Dags/spark_stream.py
@@ -0,0 +1,15 @@
+import logging
+
+from cassandra.cluster import Cluster
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType
+
+
+def create_keyspace(session):
+    session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS spark_streams
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+    """)
+
+    print("Keyspace created successfully!")
